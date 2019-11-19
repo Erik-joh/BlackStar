4343544f35c297d5b82adb8392dbd1dec949d0cd
Erik-joh <erik.johannesson0@gmail.com>
2019-11-19 18:28:42 +0100

    slideshow and scroll functions

---
+++ script/imgSlider.js
@@ -0,0 +1,6 @@
+setInterval(function(){
+    let element = document.querySelector('.img');
+    let container = document.querySelector('.imgContainer');
+    container.append(element);
+    element.classList.add('imgAnimate');
+},5000);
